3a0b357705749d7f804bfe3f2c975848267f7b72
Sparsh <sparshgoelk@gmail.com>
2026-03-27 13:59:04 +0530

    added file headers

--- src/commands/send.rs
+++ src/commands/send.rs
@@ -1,4 +1,5 @@
 use crate::pipe::pipe;
+use std::fmt::format;
 use std::fs::{self, File};
 use std::io::{self, Write};
 use std::path::Path;
@@ -13,9 +14,10 @@ pub fn execute(files: Vec<String>) -> Result<(), io::Error> {
     for path in files {
         let path_ref = Path::new(&path);
         if path_ref.is_file() {
-            send_file(path_ref, &mut stdout)?;
+            let base = path_ref.parent().unwrap_or(Path::new(""));
+            send_file(base, path_ref, &mut stdout)?;
         } else if path_ref.is_dir() {
-            send_dir(path_ref, &mut stdout)?;
+            send_dir(path_ref, path_ref, &mut stdout)?;
         } else {
             eprintln!("Invalid path {}", path)
         }
@@ -23,7 +25,7 @@ pub fn execute(files: Vec<String>) -> Result<(), io::Error> {
     Ok(())
 }
 
-fn send_file(path: &Path, stdout: &mut impl Write) -> Result<(), io::Error> {
+fn send_file(base: &Path, path: &Path, writer: &mut impl Write) -> Result<(), io::Error> {
     let mut file = match File::open(path) {
         Ok(f) => f,
         Err(e) => {
@@ -31,20 +33,32 @@ fn send_file(path: &Path, stdout: &mut impl Write) -> Result<(), io::Error> {
             return Ok(());
         }
     };
-    pipe(&mut file, stdout)?;
+    let metadata = file.metadata()?;
+    let size = metadata.len();
+
+    let rel_path = path.strip_prefix(base).unwrap();
+    let rel_str = rel_path.to_string_lossy();
+    let header = format!(
+        "FILE WITH PATH_LEN {} PATH {} SIZE {}\n",
+        rel_str.len(),
+        rel_str,
+        size
+    );
+    writer.write_all(header.as_bytes());
+    pipe(&mut file, writer)?;
     Ok(())
 }
 
-fn send_dir(path: &Path, stdout: &mut impl Write) -> Result<(), io::Error> {
+fn send_dir(base: &Path, path: &Path, stdout: &mut impl Write) -> Result<(), io::Error> {
     for entry in fs::read_dir(path)? {
         let entry = entry?;
         let entry_path = entry.path();
         if entry_path.is_file() {
-            send_file(&entry_path, stdout)?;
+            send_file(base, &entry_path, stdout)?;
         } else if entry_path.is_dir() {
-            send_dir(&entry_path, stdout)?;
+            send_dir(base, &entry_path, stdout)?;
         }
     }
     Ok(())
 }
-
+//file header FILE <path_length> <path> <size>\n
